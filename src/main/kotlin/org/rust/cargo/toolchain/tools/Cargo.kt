ed05d6b9eb2f995103b89d73e3151c8f0722043c
mchernyavsky <chernyavsky.mikhail@gmail.com>
2021-09-19 23:58:28 +0300

    RUN: Correctly handle `+toolchain` in Cargo command configuration

--- src/main/kotlin/org/rust/cargo/toolchain/tools/Cargo.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/tools/Cargo.kt
@@ -376,6 +376,8 @@ class Cargo(toolchain: RsToolchainBase, useWrapper: Boolean = false)
             val parameters = buildList<String> {
                 if (channel != RustChannel.DEFAULT) {
                     add("+$channel")
+                } else if (toolchain != null) {
+                    add(toolchain)
                 }
                 if (project.rustSettings.useOffline) {
                     val cargoProject = findCargoProject(project, additionalArguments, workingDirectory)
@@ -387,8 +389,8 @@ class Cargo(toolchain: RsToolchainBase, useWrapper: Boolean = false)
                 add(command)
                 addAll(additionalArguments)
             }
-            val rustcExecutable = toolchain.rustc().executable.toString()
-            toolchain.createGeneralCommandLine(
+            val rustcExecutable = this@Cargo.toolchain.rustc().executable.toString()
+            this@Cargo.toolchain.createGeneralCommandLine(
                 executable,
                 workingDirectory,
                 redirectInputFrom,
